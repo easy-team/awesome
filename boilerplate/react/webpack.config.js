13713cdd839a53df4f3116c6d4de9ee57797702b
sky <hubcarl@126.com>
2018-01-25 19:15:50 +0800

    refactor: react

--- boilerplate/react/webpack.config.js
+++ boilerplate/react/webpack.config.js
@@ -3,33 +3,25 @@ const path = require('path');
 module.exports = {
   type: 'client',
   framework: 'react',
+  devtoo: 'source-map',
   entry: {
     include: 'page',
     exclude: ['page/test'],
-    template: 'view/layout.html',
     loader: {
       client: 'framework/entry/loader.js'
     }
   },
+  template: 'view/layout.html',
   alias: {
     asset: 'asset',
     component: 'component',
     framework: 'framework'
   },
-  cssExtract: true,
-  options: {},
   loaders: {
     eslint: false,
     less: false,
     stylus: false
   },
-  plugins: {
-    imagemini: false
-  },
-  create() {
-    // call api custom config
-  },
-  done(){
-
-  }
+  plugins: {},
+  done(){}
 };
--- boilerplate/weex/webpack.config.js
+++ boilerplate/weex/webpack.config.js
@@ -2,11 +2,8 @@
 const path = require('path');
 module.exports = {
   framework: 'weex',
-  entry: {
-    include: 'page',
-    exclude: ['page/html'],
-    template: 'view/layout.html'
-  },
+  entry: './page',
+  template: 'view/layout.html',
   alias: {
     asset: 'asset',
     app: 'framework/vue/app.js',
@@ -14,25 +11,10 @@ module.exports = {
     framework: 'framework',
     store: 'store'
   },
-  options: {},
-  loaders: {
-    eslint: false,
-    less: false,
-    stylus: false
-  },
-  plugins: {
-    imagemini: false
-  },
-  create() {
-    // call api custom config
-  },
-  onWeb(){
-    this.addEntry('vendor', [path.join(this.config.baseDir, 'framework/weex/web.js')]);
-  },
-  onWeex(){
-
-  },
-  done(){
-
-  }
+  loaders: {},
+  plugins: {},
+  lib: [path.join(__dirname, 'framework/weex/web.js')],
+  onWeb(){},
+  onWeex(){},
+  done(){}
 };
