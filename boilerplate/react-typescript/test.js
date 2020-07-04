b61b363e5339d89502d0ecc74d819d53908188be
sky <cl98914@alibaba-inc.com>
2020-07-04 12:34:25 +0800

    test: remove test code

--- boilerplate/react-typescript/test.js
+++
@@ -1,8 +0,0 @@
-const json = require('./cuttle.json');
-
-const list = json.data.filter(item => {
-  return item.country === 'in' && item.groupId === '5y7rE6OKVR' 
-  && item.cName.indexOf('电影娱乐社区')>-1;
-}).map(item => { return item.appId;});
-
-console.log(list.length);
